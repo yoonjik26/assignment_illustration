e69b12a1c53093c37f6b79d568fb234970ebaa93
yoonjik26 <yoonjik1126@gmail.com>
2015-10-20 00:02:47 -0400

    sketch

--- illustration_p5/illustration.js
+++ illustration_p5/illustration.js
@@ -1,9 +1,59 @@
-// illustration.js
+var backgroundImage;
+var openImage;
+var carpetImage;
+var chairImage;
+var basketImage;
+var dogImage;
+var bookImage;
+var catImage;
+var blanketImage;
+var girlImage;
+
+function preload() {
+	backgroundImage = loadImage("images/background.png");
+	openImage = loadImage("images/open.png");
+	carpetImage = loadImage("images/carpet.png");
+	chairImage = loadImage("images/chair.png");
+	basketImage = loadImage("images/basket.png");
+	dogImage = loadImage("images/dog.png");
+	bookImage = loadImage("images/book.png");
+	catImage = loadImage("images/cat.png");
+	blanketImage = loadImage("images/blanket.png");
+	girlImage = loadImage("images/girl.png");
+}
 
 function setup() {
 	createCanvas(800, 600);
 }
 
 function draw() {
-	background(200,0,0);
+	image(backgroundImage, 0, 0);
+	if (mouseX < 290 && mouseX > 70 && mouseY < 420 && mouseY > 120 && mouseIsPressed) {
+		image(openImage, 0, 0);
+	}	
+	if (mouseX < 470 && mouseY > 450 && mouseY < 590) {
+		image(carpetImage, mouseX - 400, 0);
+	} else {
+		image(carpetImage, 0, 0);
+	}
+	if (mouseX > 200 && mouseX < 300 && mouseY > 430 && mouseY < 530 && mouseIsPressed) {
+		image(dogImage, 0, 0);
+	} else {
+		image(basketImage, random (- 3, 3), 0);
+	}
+	if (mouseX < 440 && mouseX > 330 && mouseY < 430 && mouseY > 250 && mouseIsPressed) {
+		image(chairImage, mouseX - 330, 0);
+	} else {
+		image(chairImage, 0, 0);
+	}
+	if (mouseX > 610 && mouseY < 150 && mouseY > 50) {
+		image(bookImage, mouseX - 600, 0);
+	} else {
+		image(bookImage, 0, 0);
+	}
+	if (mouseX > 550 && mouseX < 750 && mouseY > 310 && mouseY < 500 && mouseIsPressed) {
+		image(girlImage, 0, 0);
+	} else {
+		image(blanketImage, 0, 0);
+	}
 }
